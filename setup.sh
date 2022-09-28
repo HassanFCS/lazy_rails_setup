3b37d533fa2a35292ad7bd5151c4d097470156a7
Ibn e Hassan <44069225+HassanFCS@users.noreply.github.com>
2022-09-28 23:58:05 +0500

    formatting change

--- setup.sh
+++ setup.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
-echo "Updating system packages..."
+echo "Updating system packages...\n"
 sudo apt update
 
-echo "Upgrading system packages..."
+echo "\nUpgrading system packages...\n"
 sudo apt upgrade -y
 
-echo "Installing gnupg2..."
+echo "\nInstalling gnupg2...\n"
 sudo apt install gnupg2
 
-echo "Installing curl (in case it doesn't exist)..."
+echo "\nInstalling curl (in case it doesn't exist)...\n"
 sudo apt install curl
 
-echo "Adding rvm gpg2 key..."
+echo "\nAdding rvm gpg2 key...\n"
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-echo "Setting up rvm.sh..."
+echo "\nSetting up rvm.sh...\n"
 \curl -sSL https://get.rvm.io -o rvm.sh
 cat rvm.sh | bash -s stable
 source ~/.rvm/scripts/rvm
+rvm install 3.0.0
 
-echo "Setting up yarn..."
+echo "\nSetting up Yarn...\n"
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-echo "Setting up nvm..."
+echo "\nSetting up NVM...\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-echo "Running sudo apt update..."
+echo "\nRunning sudo apt update...\n"
 sudo apt update
 
-echo "Installing Postgres, RVM and Yarn..."
-sudo apt install -y postgresql postgresql-contrib libpq-dev software-properties-common rvm yarn
+echo "\nInstalling Postgres...\n"
+sudo apt install -y postgresql postgresql-contrib libpq-dev software-properties-common
 
-echo "Reloading bash config..."
+echo "\nInstalling Yarn...\n"
+sudo apt install -y yarn
+
+echo "\nReloading bash config...\n"
 source ~/.bashrc
 
-echo "Installling Rails 6..."
+echo "\nInstallling Rails 6...\n"
 gem install rails -v 6.1
